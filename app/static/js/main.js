5029cfaf7e08869faa50093e5e94962f028ab2ae
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-11-20 13:50:01 +0800

    Load aerial view image in canvas

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,1 +1,13 @@
+$(function () {
+  var canvas = new fabric.Canvas('map-container', {
+    width: 828,
+    height: 495
+  });
 
+
+  fabric.Image.fromURL('/static/img/aerial-view.jpg', function (img) {
+    img.set('selectable', false);
+    canvas.add(img);
+  });
+
+});
