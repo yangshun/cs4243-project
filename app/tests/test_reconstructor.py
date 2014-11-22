42020a420436dcec6b0f93aa310bc33c679d1c15
Nguyen Trung Hieu <ngtrhieu0011@gmail.com>
2014-11-22 22:20:54 +0800

    Update Reconstructor test cases

--- app/tests/test_reconstructor.py
+++ app/tests/test_reconstructor.py
@@ -1,18 +1,17 @@
 from unittest import TestCase
 from app.texture_extractor import Reconstructor
 
-
 class TestReconstructor(TestCase):
-    
-    def test_convertToVirtualPlane(self):
-        reconstructor = Reconstructor((20, 10), 1, 1)
-        self.assertTupleEqual(reconstructor.convertToVirtualPlane((0, 0)), (-10, 5))
-        self.assertTupleEqual(reconstructor.convertToVirtualPlane((5, 10)), (0, 0))
-        self.assertTupleEqual(reconstructor.convertToVirtualPlane((10, 20)), (10, -5))
-        self.assertTupleEqual(reconstructor.convertToVirtualPlane((20, 10)), (0, -15))
-        self.assertTupleEqual(reconstructor.convertToVirtualPlane((7.5, 5)), (-5, -2.5))
+	
+	def test_convertToVirtualPlane(self):
+		reconstructor = Reconstructor((10, 20), 1, 1)
+		self.assertTupleEqual(reconstructor.convertToVirtualPlane((0, 0)), (-10, 5))
+		self.assertTupleEqual(reconstructor.convertToVirtualPlane((5, 10)), (0, 0))
+		self.assertTupleEqual(reconstructor.convertToVirtualPlane((10, 20)), (10, -5))
+		self.assertTupleEqual(reconstructor.convertToVirtualPlane((20, 10)), (0, -15))
+		self.assertTupleEqual(reconstructor.convertToVirtualPlane((7.5, 5)), (-5, -2.5))
 
-    def test_calculate3DCoordinate (self):
+	def test_calculate3DCoordinate (self):
 		reconstructor = Reconstructor ((10, 20), 10, 5)
 		self.assertTupleEqual (reconstructor.calculate3DCoordinate ((5, 10), 10), (0, 0, 10))
 		self.assertTupleEqual (reconstructor.calculate3DCoordinate ((0, 0), 10), (-2, 1, 10))
